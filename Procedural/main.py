bacee2c1bb343848da9c24fb87e7487e313bff6a
Mac.Cheese <mac.cheese324@gmail.com>
2025-03-23 15:22:01 -0400

    now with images

--- Procedural/main.py
+++ Procedural/main.py
@@ -1,6 +1,6 @@
 import pygame
 import noise
-import random
+import image_reader
 
 pygame.init()
 
@@ -11,30 +11,48 @@ win = pygame.display.set_mode((600, 600))
 hit_space = False
 
 x, y = 0, 0
+tiles = image_reader.get_tileset("Procedural/images/tileset.png", 3, 3, 16, 16, 2.34375)
 
-def genNoise(width: int, height: int, seed: int) -> list[list[float]]:
+def genNoise(width: int, height: int, seed: int, detail: int) -> list[list[float]]:
+    scale = 500
     m = [[(0) for x in range(width)] for y in range(height)]
     for x in range(width):
         for y in range(height):
-            m[y][x] = noise.pnoise2((x + 1) / 200, (y + 1) / 200, 6, 0.6, 4, base = seed)
+            m[y][x] = noise.pnoise2(x / scale, y / scale, detail, 0.6, 4, base = seed)
     return m
 
+def getColor(val: float) -> pygame.Color:
+    if val < 0:
+        return (0, 0, 255)
+    else:
+        if val < 0.16:
+            return (0, 255, 0)
+        else:
+            return (40, 40, 40)
+
+def getImage(val: float) -> pygame.Surface:
+    if val < 0:
+        return tiles[1]
+    else:
+        if val < 0.16:
+            return tiles[0]
+        else:
+            return tiles[2]
+
 def getImageFromNoise(terrain: list[list[float]], width: int, height: int) -> pygame.Surface:
-    image = pygame.Surface((len(terrain[0]), len(terrain)))
+    image = pygame.Surface((width, height))
     for y in range(len(terrain)):
         for x in range(len(terrain[y])):
-            if terrain[y][x] > 0:
-                val = (0, 255, 0)
-            else:
-                val = (0, 0, 255)
-            image.set_at((x, y), val)
-    image = pygame.transform.scale(image, (width, height))
+            tile = getImage(terrain[y][x])
+            image.blit(tile, (tile.get_width() * x, tile.get_height() * y))
     return image
 
-n = genNoise(600, 600, random.randint(0, 100))
+n = genNoise(16, 16, 100, 5)
 image = getImageFromNoise(n, 600, 600)
 
+
 time = pygame.time.Clock()
+seed = 0
 
 run = True
 while run:
@@ -44,7 +62,8 @@ while run:
     
     k = pygame.key.get_pressed()
     if k[pygame.K_SPACE] and not hit_space:
-        n = genNoise(600, 600, random.randint(0, 100))
+        seed += 1
+        n = genNoise(16, 16, seed + 100, 5)
         image = getImageFromNoise(n, 600, 600)
         hit_space = True
     if not k[pygame.K_SPACE]:
